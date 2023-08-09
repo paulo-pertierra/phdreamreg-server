1783f9456878baac7f27301a0393cc2604fbbe28
Paulo Pertierra <paulopertierra@gmail.com>
2023-08-09 10:08:38 +0800

    Add health check, index page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import compression from "compression";
 import dotenv from "dotenv";
+
+
 dotenv.config();
 
 const app = express();
@@ -7,10 +10,23 @@ const app = express();
 import cors from "cors";
 
 app.use(cors());
+app.use(compression())
 app.use(express.json())
+app.use(express.static(__dirname + "/../public"))
+console.log(__dirname + "/public")
 
 import { apiRouter } from "./modules/apiRouter";
 app.use("/api", apiRouter)
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/pages/index.html");
+})
+
+app.use("/healthz", (req, res) => {
+  res.status(200).json({
+    ping: "pong"
+  })
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
