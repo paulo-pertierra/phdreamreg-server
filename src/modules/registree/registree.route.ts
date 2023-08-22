c0c98e51115d03eafe1f43e23ca6b53f7d9ead5c
Paulo Pertierra <paulopertierra@gmail.com>
2023-08-22 10:44:33 +0800

    Add get registree with ID endpoint

--- src/modules/registree/registree.route.ts
+++ src/modules/registree/registree.route.ts
@@ -6,7 +6,7 @@ import { validateAdminToken } from '../../middlewares/jwtverification.handler';
 export const registreeRouter = Router();
 
 registreeRouter.get('/', validateAdminToken, registreeController.getRecentRegistrees);
-// registreeRouter.get('/:id', validateAdminToken, registreeController.getUniqueRegistree);
+registreeRouter.get('/:id', validateAdminToken, registreeController.getUniqueRegistree);
 registreeRouter.post('/', createRegistreeValidator, registreeController.createRegistree);
 registreeRouter.put(
   '/:id',
